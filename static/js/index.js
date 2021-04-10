b6fc5f2ee9661dc8854849f00e52af2c7e280497
juank1904 <76795308+juank1904@users.noreply.github.com>
2021-04-10 11:55:55 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -46,8 +46,9 @@ function LED1_Off(){
 	
     client.subscribe("juanpinduisaca.jq@gmail.com/test");
     message = new Paho.MQTT.Message("hola desde la web");
-    message1 = new Paho.MQTT.Message("hola juank");
     message.destinationName = "juanpinduisaca.jq@gmail.com/test";
+    client.subscribe("juanpinduisaca.jq@gmail.com/test");
+    message1 = new Paho.MQTT.Message("hola juank");
     message1.destinationName = "juanpinduisaca.jq@gmail.com/test";
     client.send(message);
 	
