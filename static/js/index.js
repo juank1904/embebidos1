8edf2aa65a8afce475df42bf1032223c66a59f8a
juank1904 <76795308+juank1904@users.noreply.github.com>
2021-04-10 09:36:20 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -69,7 +69,7 @@ function LED1_Off(){
   // called when a message arrives
   function onMessageArrived(message) {
     console.log("onMessageArrived:"+message.payloadString);
-	  document.getElementById("sensor").innerHTML=message.payloadString;
-	  document.getElementById("sensor1").innerHTML=message.payloadString;
+	  document.getElementById("sensor_led").innerHTML=message.payloadString;
+	  document.getElementById("sensor_motor").innerHTML=message.payloadString;
   }
   
