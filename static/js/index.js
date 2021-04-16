c06925e1f1d3b8b244a53b899dc9684a833212ae
juank1904 <76795308+juank1904@users.noreply.github.com>
2021-04-16 17:04:41 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
 //https://www.eclipse.org/paho/clients/js/
 
-function LED1_On() {
+function historial_1(){
 	//alert("led on");
 	console.log("led on");
 	//document.getElementById("sensor").innerHTML="led on";
@@ -9,7 +9,7 @@ function LED1_On() {
     	client.send(message);
   
 }
-function LED1_Off(){	
+function historial_1(){	
 	//alert("led off");
 	console.log("led off");
 	message = new Paho.MQTT.Message("0");
